ae185f21208bf6cdc1d994567e52f25957dc45c6
SaadQayyum-9 <saadqayyum09@gmail.com>
2022-07-08 19:44:04 +0500

    updated verson of rest api, new features added: update and delete

--- client/app.js
+++ client/app.js
@@ -7,11 +7,11 @@ var HttpRequest = require("xmlhttprequest").XMLHttpRequest;
 function encode(params, prefix = "") {
 
     var items = [];
-    
-    for(var field in params) {
-        var key  = prefix ? prefix + "[" + field + "]" : field;
+
+    for (var field in params) {
+        var key = prefix ? prefix + "[" + field + "]" : field;
         var type = typeof params[field];
-        if (type === "object"){
+        if (type === "object") {
             items.push(encode(params[field], key));
         } else {
             items.push(key + "=" + encodeURIComponent(params[field]));
@@ -20,15 +20,17 @@ function encode(params, prefix = "") {
 
     return items.join("&");
 }
-function getPakistanCricketTeam(){
+async function getCricketTeam(country) {
     let request = new HttpRequest();
-    request.open("GET", "http://localhost:3000/data/pakistan");
+    request.open("GET", "http://localhost:3000/show?country=" + country);
     request.send();
     request.onload = () => {
-       console.log(JSON.parse(request.responseText));
+        console.log(JSON.parse(request.responseText));
+        return;
     }
+    await new Promise(r => setTimeout(r, 1500));
 }
-function getData(matchWon = {}, matchLost = {}, matchPlayed = {}, sortBy = undefined){
+async function getData(matchWon = {}, matchLost = {}, matchPlayed = {}, sortBy = undefined) {
     let queryParams = {
         "matches-won": matchWon,
         "matches-lost": matchLost,
@@ -39,23 +41,79 @@ function getData(matchWon = {}, matchLost = {}, matchPlayed = {}, sortBy = undef
     request.open("GET", "http://localhost:3000/data?" + encode(queryParams));
     request.send();
     request.onload = () => {
-       console.log(JSON.parse(request.responseText));
+        console.log(JSON.parse(request.responseText));
+        return;
     }
+    await new Promise(r => setTimeout(r, 1500));
 }
-getPakistanCricketTeam();    //uncomment this fucntion to display pakistan cricket team
 
-let matchWon = {
-    gte: 150,
-    //lte: 500
-};
-let matchLost = {
-  //  gte: 50,
-    lte: 500
-};
-let matchPlayed = {
-    gte: 50,
-    lte: 900
-};
-let sort = "country";
+async function addCountry(country) {
+    let request = new HttpRequest();
+    request.open("POST", "http://localhost:3000/add?" + encode(country));
+    request.send();
+    request.onload = () => {
+        console.log(request.responseText);
+        return;
+    }
+    await new Promise(r => setTimeout(r, 1500));
+}
 
-getData(matchWon, matchLost, matchPlayed, sort);
+async function updateCountry(countryName, updatedCountry) {
+    let queryParam = {
+        "countryName": countryName,
+        "updatedCountry": updatedCountry
+    }
+    let request = new HttpRequest();
+    request.open("PUT", "http://localhost:3000/update?" + encode(queryParam));
+    request.send();
+    request.onload = () => {
+        console.log(request.responseText);
+        return;
+    }
+    await new Promise(r => setTimeout(r, 1500));
+}
+
+async function deleteCountry(country) {
+    let request = new HttpRequest();
+    request.open("DELETE", "http://localhost:3000/delete?country=" + country);
+    request.send();
+    request.onload = () => {
+        console.log(request.responseText);
+        return;
+    }
+    await new Promise(r => setTimeout(r, 1500));
+}
+
+// Application
+async function runApplication() {
+    await getCricketTeam("Pakistan"); // showing Pakistan cricket team
+    await addCountry({ // adding a new country "My Country"
+        "country": "My Country",
+        "team-matches-played": 7,
+        "matches-won": 3,
+        "matches-lost": 4
+    });
+    await getCricketTeam("My Country");
+    await updateCountry("My Country", {  // update the country given
+        "country": "My Country 2"
+    });
+    await getCricketTeam("My Country 2");
+    await deleteCountry("My Country 2"); // delete the country given
+    await getCricketTeam("My Country 2");
+    let matchWon = {
+        gte: 150,
+        //lte: 500
+    };
+    let matchLost = {
+        //gte: 50,
+        lte: 500
+    };
+    let matchPlayed = {
+        gte: 50,
+        lte: 900
+    };
+    let sort = "country"; // sort alphabetically of countries
+    await getData(matchWon, matchLost, matchPlayed, sort); // apply all filters and display data
+}
+
+runApplication();
--- server/server.js
+++ server/server.js
@@ -1,12 +1,107 @@
+const { count } = require('console');
+const { response } = require('express');
 const express = require('express');
 const fs = require('fs');
+const { get } = require('http');
 const app = express();
 const PORT = 3000;
-  
+
 var dataset;
 
+fs.readFile('Assignment_Dataset.json', (err, data) => {
+    if (err) throw err;
+    dataset = JSON.parse(data.toString());
+
+})
+function writeFile(res, msg) {
+    fs.writeFile('Assignment_Dataset.json', JSON.stringify(dataset, undefined, 2), err => {
+        if (err) {
+            res.status(400);
+            res.send(err);
+            return
+        } else {
+            res.status(200);
+            res.send(msg);
+        }
+    });
+}
+
+app.get('/show', (req, res) => {
+    console.log(req.path)
+    let country;
+
+    for (let x in dataset) {
+        if (dataset[x].country === req.query["country"]) {
+            country = dataset[x];
+            break;
+        }
+    } 
+    if(country == undefined) {
+        res.status(400);
+        res.send({"error": "Country not found"});
+    } else {
+        res.status(200);
+        res.send(country);
+    }
+});
+
+app.post('/add', (req, res) => {
+    console.log(req.path)
+
+    if (req.query["country"] != undefined &&
+        req.query["team-matches-played"] != undefined &&
+        req.query["matches-won"] != undefined &&
+        req.query["matches-lost"] != undefined
+    ) {
+        dataset.push({
+            "country": req.query["country"],
+            "team-matches-played": parseInt(req.query["team-matches-played"]),
+            "matches-won": parseInt(req.query["matches-won"]),
+            "matches-lost": parseInt(req.query["matches-lost"])
+        });
+        writeFile(res, "Successfully Added");
+    } else {
+        res.status(400);
+        res.send("Wrong data received");
+    }
+});
+
+app.put('/update', (req, res) => {
+    console.log(req.path)
+    let found = false;
+    for (let x in dataset) {
+        if (dataset[x].country === req.query["countryName"]) {
+            found = true;
+            dataset[x] = {
+                "country": req.query["updatedCountry"]["country"] ?? dataset[x]["country"],
+                "team-matches-played": parseInt(req.query["updatedCountry"]["team-matches-played"] ?? dataset[x]["team-matches-played"]),
+                "matches-won": parseInt(req.query["updatedCountry"]["matches-won"] ?? dataset[x]["matches-won"]),
+                "matches-lost": parseInt(req.query["updatedCountry"]["matches-lost"] ?? dataset[x]["matches-lost"])
+            };
+            break;
+        }
+    }
+    if (found) {
+        writeFile(res, "Successfully Updated");
+    } else {
+        res.status(400);
+        res.send("Country not found");
+    }
+});
+
+app.delete('/delete', (req, res) => {
+    console.log(req.path)
+    res.status(200);
+    for (let x in dataset) {
+        if (dataset[x].country === req.query["country"]) {
+            dataset.splice(x, 1);
+        }
+    }
+    writeFile(res, "Successfully Deleted all values with country name: " + req.query["country"]);
+});
+
 function GetSortOrder(prop) {
-    return function(a, b) {
+    return function (a, b) {
         if (a[prop] > b[prop]) {
             return 1;
         } else if (a[prop] < b[prop]) {
@@ -14,45 +109,22 @@ function GetSortOrder(prop) {
         }
         return 0;
     }
-} 
-fs.readFile('Assignment_Dataset.json', (err, data) => {
-    if (err) throw err;
-    dataset = JSON.parse(data.toString());
-    
-})
+}
 
-app.get('/data/pakistan', (req, res)=>{
+app.get('/data', (req, res) => {
     console.log(req.path)
-    res.status(200);
-    var pakistan;
-
-    for (let x in dataset) {
-        if (dataset[x].country === "Pakistan") {
-            pakistan = dataset[x];
-            break;
-        }
-    }
-    res.send(pakistan);
-    
-});
-
-app.get('/data', (req, res)=>{
-    console.log(JSON.stringify(req.query["matches-won"]));
-    console.log(JSON.stringify(req.query["matches-lost"]));
-    console.log(JSON.stringify(req.query["team-matches-played"]));
-    console.log(JSON.stringify(req.query["sort"]));
     var sort = req.query["sort"];
 
     var wonGte = req.query["matches-won"]?.gte ?? 0;
     var wonLte = req.query["matches-won"]?.lte ?? 10000;
     var lostGte = req.query["matches-lost"]?.gte ?? 0;
-    var lostLte = req.query["matches-lost"]?.lte ?? 10000;   
+    var lostLte = req.query["matches-lost"]?.lte ?? 10000;
     var playedGte = req.query["team-matches-played"]?.gte ?? 0;
     var playedLte = req.query["team-matches-played"]?.lte ?? 10000;
 
     var selected = [];
     for (let x in dataset) {
-        if (dataset[x]["matches-won"] >= wonGte && 
+        if (dataset[x]["matches-won"] >= wonGte &&
             dataset[x]["matches-won"] <= wonLte &&
             dataset[x]["matches-lost"] >= lostGte &&
             dataset[x]["matches-lost"] <= lostLte &&
@@ -61,18 +133,18 @@ app.get('/data', (req, res)=>{
             selected.push(dataset[x]);
         }
     }
-    if(sort !== undefined){
+    if (sort !== undefined) {
         selected.sort(GetSortOrder(sort))
     }
-    
+
     res.status(200);
     res.send(selected);
 });
 
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running and App is listening on port "+ PORT)
-    else 
+app.listen(PORT, (error) => {
+    if (!error)
+        console.log("Server is Successfully Running and App is listening on port " + PORT)
+    else
         console.log("Error occurred, server can't start", error);
-    }
+}
 );
